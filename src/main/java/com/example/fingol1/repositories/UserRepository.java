0bbd4d25b53b2b667f630829441336ba575b0405
szymeq17 <rysz.szymon@gmail.com>
2022-03-02 16:17:47 +0100

    Add more methods to repository

--- src/main/java/com/example/fingol1/repositories/UserRepository.java
+++ src/main/java/com/example/fingol1/repositories/UserRepository.java
@@ -3,33 +3,61 @@ package com.example.fingol1.repositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 @Repository
 public class UserRepository {
     private HashMap<String, Integer> users = new HashMap<>();
 
-    public HashMap<String, Integer> getUsers() {
-        return users;
+
+    public void registerUser(String name) {
+        if (users.containsKey(name)) {
+            users.replace(name, users.get(name) + 1);
+        } else {
+            users.put(name, 1);
+        }
     }
 
-    public void registerUser(String user) {
-        if (users.containsKey(user)) {
-            users.replace(user, users.get(user) + 1);
-        } else {
-            users.put(user, 1);
-        }
+    public void deleteUser(String name) {
+        users.remove(name);
     }
 
     public int getUserRegistrations(String user) {
         return users.get(user);
     }
 
+    public LinkedHashMap<String, Integer> getAllUsers() {
+        return users.entrySet()
+                .stream()
+                .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Entry::getKey,
+                        Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new));
+    }
+
     public LinkedHashMap<String, Integer> getTop3Users() {
         return users.entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(3)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                .collect(Collectors.toMap(
+                        Entry::getKey,
+                        Entry::getValue,
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new));
+    }
+
+    public LinkedHashMap<String, Integer> getAllUsersIgnoreCase() {
+        return users.entrySet()
+                .stream()
+                .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey().toUpperCase(),
+                        Entry::getValue,
+                        Integer::sum,
+                        LinkedHashMap::new));
     }
 }
