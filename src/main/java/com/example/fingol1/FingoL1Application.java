bd14c66374e813d72b81fe25402468cd8ef8913c
szymeq17 <rysz.szymon@gmail.com>
2022-03-02 13:57:50 +0100

    Initial commit

---
+++ src/main/java/com/example/fingol1/FingoL1Application.java
@@ -0,0 +1,13 @@
+package com.example.fingol1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingoL1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingoL1Application.class, args);
+    }
+
+}
